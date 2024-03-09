026351f6f1be2e1bd4026c51bb1b32a3afe146e0
chenxin <1692856735@qq.com>
2024-03-09 21:32:04 +0800

    基础环境搭建完成

--- api_platform/src/main/java/com/chen/Platform.java
+++ api_platform/src/main/java/com/chen/Platform.java
@@ -1,74 +1,21 @@
 package com.chen;
 
-import com.chen.common.utils.LogUtil;
-import com.chen.interceptor.JwtTokenAdminInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @EnableWebMvc  //springMVC
 @Configuration  //启用配置模式
 @EnableScheduling  //定时任务
 @MapperScan("com.chen.core.*.mapper")  //扫描mapper文件包
 @SpringBootApplication(scanBasePackages = {"com.chen"})
-public class Platform  extends WebMvcConfigurationSupport {
+public class Platform {
 
     public static void main(String[] args) {
         SpringApplication.run(Platform.class, args);
-    }
-
-    @Autowired
-    private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
-
-    /**
-     * 注册自定义拦截器
-     *
-     * @param registry 拦截器注册器
-     */
-    protected void addInterceptors(InterceptorRegistry registry) {
-        LogUtil.printInfo("开始注册自定义拦截器...");
-        registry.addInterceptor(jwtTokenAdminInterceptor)
-                .addPathPatterns("/admin/**")
-                .excludePathPatterns("/admin/employee/login");
-    }
-
-
-    /** 跨域配置 */
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addExposedHeader("3600");
-        return corsConfiguration;
-    }
-
-    /** 跨域过滤器 */
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig());
-        return new CorsFilter(source);
-    }
-
-    /**
-     * 设置静态资源映射
-     * @param registry
-     */
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
---
+++ api_platform/src/main/java/com/chen/config/WebMvcConfiguration.java
@@ -0,0 +1,64 @@
+package com.chen.config;
+
+import com.chen.common.utils.LogUtil;
+import com.chen.interceptor.JwtTokenAdminInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * 配置类，注册web层相关组件
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Autowired
+    private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
+
+    /**
+     * 注册自定义拦截器
+     *
+     * @param registry 拦截器注册器
+     */
+    protected void addInterceptors(InterceptorRegistry registry) {
+        LogUtil.printInfo("开始注册自定义拦截器...");
+        registry.addInterceptor(jwtTokenAdminInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/employee/login");
+    }
+
+    @Bean
+    public Docket docket() {
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("接口文档")
+                .version("1.0")
+                .description("接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.chen.platform"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
